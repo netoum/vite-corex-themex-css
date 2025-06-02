422d255b796dd9087bb2ed98f0d66708bc278168
karimsemmoud <karim.semmoud@netoum.com>
2025-06-02 23:58:38 +0200

    0.0.2

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import './style.css'
+
+import "@netoum/corex";
+
+// import "@netoum/corex/accordion";
+// import "@netoum/corex/avatar";
+// import "@netoum/corex/checkbox";
+// import "@netoum/corex/clipboard";
+// import "@netoum/corex/collapsible";
+// import "@netoum/corex/dialog";
+// import "@netoum/corex/listbox";
+// import "@netoum/corex/menu";
+// import "@netoum/corex/switch";
+// import "@netoum/corex/switcher";
+// import "@netoum/corex/tabs";
+// import "@netoum/corex/toggle-group";
+// import "@netoum/corex/tree-view";
+
+
